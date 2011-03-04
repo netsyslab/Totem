33f0885f33f5f0250cf72d0ef72a0985313d8be9
elizeu <elsantosneto@gmail.com>
2011-03-04 05:26:40 +0000

    Fix the edge sorting and add direction tag. Issue: http://codereview-netsyslab.appspot.com/26001/

--- scripts/txt2totem.sh
+++ scripts/txt2totem.sh
@@ -5,19 +5,37 @@
 # <destination node id> [weight], these columns are processed and
 # converted into an edge list where the nodes are assumed to have
 # contiguous IDs.
+#
+# Created on: 2011-03-02
+# Author: Elizeu Santos-Neto (elizeus@ece.ubc.ca)
+#
 
-if [ $# -ne 1 ]; then
-    echo "Usage: " $0 " <txt graph input>"
+if [ $# -ne 2 ]; then
+    echo "Usage: " $0 " <txt graph input> [-u]"
     exit 1
 fi
 
 # Input file
 TXT=$1
+if [ ! -f $TXT ]; then
+    echo "Could not open the file: " $TXT
+    exit 1
+fi
+
+# Make sure it is UNIX txt format.
+dos2unix $TXT 1>2&> /dev/null
 
 # Compute number of nodes/edges in the input graph and add a header.
 echo "#Nodes: " `grep -v \# $TXT | awk '{ print $1; print $2; }' | sort -nu |\
    wc -l`
 echo "#Edges: " `grep -v \# $TXT | wc -l`
+
+# Set the graph direction according to the command line parameter.
+if [ $2 == "-u" ]; then
+    echo "# Undirected"
+else 
+    echo "# Directed"
+fi 
 
 # Current number of nodes (i.e., the next node id).
 NNODES=0
@@ -46,7 +64,7 @@ for EDGE in `grep -v \# $TXT`; do
   fi
   WEIGHT=`echo $EDGE | awk '{ print $3 }'`
   echo ${hashmap[$SRC]} ${hashmap[$DST]} $WEIGHT
-done
+done | sort --key=1,1n --key=2n
 
 # Reset the field separator
 IFS=$OLD_IFS
