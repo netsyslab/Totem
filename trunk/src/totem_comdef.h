d9f5805d17a209228d7e741a145d01937910cc0f
ahgharaibeh <ahgharaibeh@b13f6db2-a700-491f-84d0-85bdae213c3a>
2011-03-08 05:45:46 +0000

    minor modifications to the thread and block configuration macros

--- trunk/src/totem_comdef.h
+++ trunk/src/totem_comdef.h
@@ -10,13 +10,14 @@
 
 // system includes
 #include <assert.h>
+#include <ctype.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <ctype.h>
+#include <unistd.h>
 
 /**
  *  Function return code types.
@@ -83,7 +84,7 @@ typedef struct options_s {
 } options_t;
 
 /**
- * Used to define local (i.e., private) functions and variables
+ * Used to define private functions and variables
  */
 #define PRIVATE static
 
@@ -96,19 +97,31 @@ const uint32_t INFINITE = UINT_MAX;
 /**
  * A macro that determines the number of threads per block passed to a kernel.
  */
-#define THREADS_PER_BLOCK 1024
+#define THREADS_PER_BLOCK 512
 
 /**
- * Computes the total number of blocks.
+ * A macro that determines the maximum number of blocks. Currently it assumes
+ * that the grid is just 1D.
  */
-#define TOTAL_BLOCKS(vertices_count) (vertices_count % THREADS_PER_BLOCK == 0) ?\
-                                      vertices_count / THREADS_PER_BLOCK :\
-                                      vertices_count / THREADS_PER_BLOCK + 1
+#define MAX_BLOCK_COUNT 1024
+
+/**
+ * A macro that determines the maximum number of threads a kernel will be 
+ * configured with.
+ */
+#define MAX_THREAD_COUNT (MAX_BLOCK_COUNT * THREADS_PER_BLOCK)
+
+/**
+ * Computes the total number of blocks. It assumes a 1D grid.
+ */
+#define TOTAL_BLOCKS(vertex_count) (((vertex_count) % THREADS_PER_BLOCK == 0) ? \
+                                    (vertex_count) / THREADS_PER_BLOCK : \
+                                    (vertex_count) / THREADS_PER_BLOCK + 1)
 
 /**
  * Computes the thread id while taking into account the block id and dimenstion
  */
-#define THREAD_GLOBAL_INDEX threadIdx.x + blockDim.x                 \
-                            * (gridDim.x * blockIdx.y + blockIdx.x)
+#define THREAD_GLOBAL_INDEX (threadIdx.x + blockDim.x                   \
+                             * (gridDim.x * blockIdx.y + blockIdx.x))
 
 #endif  // TOTEM_COMDEF_H
