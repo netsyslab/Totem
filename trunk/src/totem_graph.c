23ca5f60896e8a128e57d7d562735629bd1a50ba
elizeu <elizeu@b13f6db2-a700-491f-84d0-85bdae213c3a>
2011-03-12 00:14:18 +0000

    Define new types for edge and vertex ids. Modifies the graph_t definition to comply with the new types.

--- trunk/src/totem_graph.c
+++ trunk/src/totem_graph.c
@@ -9,7 +9,6 @@
 #include "totem_graph.h"
 #include "totem_mem.h"
 
-
 // common logistics for parsing
 #define MAX_LINE_LENGTH      100
 #define DEFAULT_EDGE_WEIGHT  0
@@ -24,8 +23,8 @@ PRIVATE const char delimiters[] = " \t\n:";
  * @param[out] directed set to true if directed
  * @return generic success or failure
  */
-PRIVATE error_t parse_metadata(FILE* file_handler, uint32_t* vertex_count, 
-                               uint32_t* edge_count, bool* directed) {
+PRIVATE error_t parse_metadata(FILE* file_handler, uint64_t* vertex_count,
+                               uint64_t* edge_count, bool* directed) {
 
   // logistics for parsing
   char          line[MAX_LINE_LENGTH];
@@ -54,8 +53,8 @@ PRIVATE error_t parse_metadata(FILE* file_handler, uint32_t* vertex_count,
   // indicates which keywords we got
   bool keywords_found[KEYWORD_COUNT] = {false, false, false, false};
 
-  /* get the metadata, the vertex and edge counts and whether the graph is 
-     directed or not. The assumption is that the metadata exists is the 
+  /* get the metadata, the vertex and edge counts and whether the graph is
+     directed or not. The assumption is that the metadata exists is the
      first three lines with the following format:
      # Nodes: vertex_count
      # Edges: edge_count
@@ -64,7 +63,7 @@ PRIVATE error_t parse_metadata(FILE* file_handler, uint32_t* vertex_count,
   while (metadata_lines--) {
     // get the line
     CHECK_ERR(fgets(line, sizeof(line), file_handler) != NULL, err_format);
-   
+
     // must be a comment
     CHECK_ERR(line[0] == '#', err_format);
 
@@ -77,7 +76,7 @@ PRIVATE error_t parse_metadata(FILE* file_handler, uint32_t* vertex_count,
       if (strcmp(token, keywords[keyword]) == 0) {
         keywords_found[keyword] = true;
         break;
-      } 
+      }
     }
     CHECK_ERR(keyword != KEYWORD_COUNT, err_format);
 
@@ -112,78 +111,81 @@ PRIVATE error_t parse_metadata(FILE* file_handler, uint32_t* vertex_count,
   return FAILURE;
 }
 
-error_t graph_initialize(const char* graph_file, bool weighted, 
+error_t graph_initialize(const char* graph_file, bool weighted,
                          graph_t** graph) {
 
 
-  /* we had to define those variables here, not within the code, to overcome a 
+  /* we had to define those variables here, not within the code, to overcome a
      compilation problem with using "goto" (used to emulate exceptions). */
-  uint32_t vertex_index = 0;
-  uint32_t edge_index   = 0;
-  uint32_t vertex_count = 0;
-  uint32_t edge_count   = 0;
+  id_t vertex_index     = 0;
+  id_t edge_index       = 0;
+  uint64_t vertex_count = 0;
+  uint64_t edge_count   = 0;
   graph_t* mygraph      = NULL;
   bool     directed     = true;
 
   // logistics for parsing
   char          line[MAX_LINE_LENGTH];
   char*         token       = NULL;
-  uint32_t      line_number = 0;
+  uint64_t      line_number = 0;
 
   assert(graph_file);
   FILE* file_handler = fopen(graph_file, "r");
   CHECK_ERR(file_handler != NULL, err_openfile);
 
-  CHECK_ERR(parse_metadata(file_handler, &vertex_count, 
+  CHECK_ERR(parse_metadata(file_handler, &vertex_count,
                            &edge_count, &directed) == SUCCESS, err);
 
   /* allocate graph buffers, we allocate an extra slot in the vertices array to
      make it easy to calculate the number of neighbors of the last vertex. */
   mygraph           = (graph_t*)malloc(sizeof(graph_t));
   assert(mygraph);
-  mygraph->vertices = (uint32_t*)mem_alloc((vertex_count + 1) * 
-                                           sizeof(uint32_t));
-  mygraph->edges    = (uint32_t*)mem_alloc(edge_count * sizeof(uint32_t));
-  mygraph->weights  = weighted? 
-    (int32_t*)mem_alloc(edge_count * sizeof(uint32_t)): NULL;
+  mygraph->vertices = (id_t*)mem_alloc((vertex_count + 1) * sizeof(id_t));
+  mygraph->edges    = (id_t*)mem_alloc(edge_count * sizeof(id_t));
+  mygraph->weights  = weighted ?
+    (weight_t*)mem_alloc(edge_count * sizeof(weight_t)) : NULL;
 
   // read line by line to create the graph
   while (fgets(line, sizeof(line), file_handler) != NULL) {
     // we got a new line
-    line_number++;    
+    line_number++;
 
     // comments start with '#', skip them
     if (line[0] == '#')
       continue;
-    
+
     // start tokenizing: first, the source node
     CHECK_ERR((token = strtok(line, delimiters)) != NULL, err_format_clean);
     CHECK_ERR(is_numeric(token), err_format_clean);
-    uint32_t src_id = (uint32_t)atoi(token);
+    uint64_t token_num  = atoll(token);
+    CHECK_ERR((token_num > ID_MAX), err_id_overflow);
+    id_t src_id = token_num;
 
     // second, the destination node
     CHECK_ERR((token = strtok(NULL, delimiters)) != NULL, err_format_clean);
     CHECK_ERR(is_numeric(token), err_format_clean);
-    uint32_t dst_id = (uint32_t)atoi(token);
-    
+    token_num  = atoll(token);
+    CHECK_ERR(token_num > ID_MAX, err_id_overflow);
+    id_t dst_id = token_num;
+
     // third, get the weight if any
-    int32_t weight = DEFAULT_EDGE_WEIGHT;
+    weight_t weight = DEFAULT_EDGE_WEIGHT;
     if (weighted && ((token = strtok(NULL, delimiters)) != NULL)) {
       // TODO(abdullah): isnumeric returns false for negatives, fix this.
       //CHECK_ERR(is_numeric(token), err_format_clean);
-      weight = atoi(token);
-    }    
+      weight = (weight_t)atof(token);
+    }
 
     if (src_id != vertex_index - 1) {
       // add new vertex
 
       // vertices must be in increasing order and less than the maximum count
-      CHECK_ERR(((src_id >= vertex_index) && (src_id < vertex_count)), 
+      CHECK_ERR(((src_id >= vertex_index) && (src_id < vertex_count)),
                 err_format_clean);
 
-      /* IMPORTANT: vertices without edges have the same index in the vertices 
+      /* IMPORTANT: vertices without edges have the same index in the vertices
          array as their next vertex, hence their number of edges as zero would
-         be calculated in the same way as every other vertex. hence the 
+         be calculated in the same way as every other vertex. hence the
          following loop. */
       while (vertex_index <= src_id) {
         mygraph->vertices[vertex_index++] = edge_index;
@@ -224,6 +226,9 @@ error_t graph_initialize(const char* graph_file, bool weighted,
   fprintf(stderr, "Can't open file %s\n", graph_file);
   graph_finalize(mygraph);
   goto err;
+ err_id_overflow:
+  fprintf(stderr, "The type used for vertex ids does not support the range of"
+          "values in this file.\n");
  err_format_clean:
   graph_finalize(mygraph);
   fprintf(stderr, "Incorrect file format at line number %d, check the file "
