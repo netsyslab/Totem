2c01a695826f3a7f6bca3ca11bd7bf35917014b9
Abdullah Gharaibeh <ahgharaibeh@users.noreply.github.com>
2011-03-24 04:07:56 +0000

    Added a new test for the helpers function for a star graph. also, added checks on the error returned by the graph initialize and finalize functions.

--- src/test/totem_helpers_unittest.cc
+++ src/test/totem_helpers_unittest.cc
@@ -13,26 +13,38 @@
 TEST(GraphHelper, Initialize) {
   graph_t* graph;
 
-  graph_initialize(DATA_FOLDER("single_node.totem"), false, &graph);
+  EXPECT_EQ(SUCCESS, graph_initialize(DATA_FOLDER("single_node.totem"), 
+                                      false, &graph));
   EXPECT_EQ((uint32_t)1, graph->vertex_count);
   EXPECT_EQ((uint32_t)0, graph->edge_count);
-  graph_finalize(graph);
+  EXPECT_EQ(SUCCESS, graph_finalize(graph));
 
-  graph_initialize(DATA_FOLDER("single_node_loop.totem"), false, &graph);
+  EXPECT_EQ(SUCCESS, 
+            graph_initialize(DATA_FOLDER("single_node_loop.totem"), 
+                             false, &graph));
   EXPECT_EQ((uint32_t)1, graph->vertex_count);
   EXPECT_EQ((uint32_t)1, graph->edge_count);
-  graph_finalize(graph);
+  EXPECT_EQ(SUCCESS, graph_finalize(graph));
 
-  graph_initialize(DATA_FOLDER("chain_1000_nodes.totem"), false, &graph);
+  EXPECT_EQ(SUCCESS, graph_initialize(DATA_FOLDER("chain_1000_nodes.totem"),
+                                      false, &graph));
   EXPECT_EQ((uint32_t)1000, graph->vertex_count);
   EXPECT_EQ((uint32_t)1998, graph->edge_count);
-  graph_finalize(graph);
+  EXPECT_EQ(SUCCESS, graph_finalize(graph));
 
-  graph_initialize(DATA_FOLDER("complete_graph_300_nodes.totem"), false, 
-                   &graph);
+  EXPECT_EQ(SUCCESS, 
+            graph_initialize(DATA_FOLDER("complete_graph_300_nodes.totem"),
+                             false, &graph));
   EXPECT_EQ((uint32_t)300, graph->vertex_count);
   EXPECT_EQ((uint32_t)89700, graph->edge_count);
-  graph_finalize(graph);
+  EXPECT_EQ(SUCCESS, graph_finalize(graph));
+
+  EXPECT_EQ(SUCCESS, 
+            graph_initialize(DATA_FOLDER("star_1000_nodes.totem"), 
+                             false, &graph));
+  EXPECT_EQ((uint32_t)1000, graph->vertex_count);
+  EXPECT_EQ((uint32_t)1998, graph->edge_count);
+  EXPECT_EQ(SUCCESS, graph_finalize(graph));
 
   // TODO(lauro, abdullah): Add more cases and test other fields.
 }
