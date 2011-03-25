534b5ca912c0f05442a5f6a8efb40af777f26d58
ahgharaibeh <ahgharaibeh@b13f6db2-a700-491f-84d0-85bdae213c3a>
2011-03-25 00:33:19 +0000

    Factored out a couple of common functions to initialize/finalize graph structure on the GPU. Also included a sample use case in page rank. The functions has been added to comkernel for the time being.

--- trunk/src/totem/totem_comkernel.cuh
+++ trunk/src/totem/totem_comkernel.cuh
@@ -10,9 +10,10 @@
 
 // totem includes
 #include "totem_comdef.h"
+#include "totem_graph.h"
 
 /**
- * a templatized version of memset for device buffers, the assumption is that
+ * A templatized version of memset for device buffers, the assumption is that
  * the caller will dispatch a number of threads at least equal to "size"
  * @param[in] buffer the buffer to be set
  * @param[in] value the value the buffer elements are set to
@@ -28,6 +29,81 @@ __global__ void memset_device(T* buffer, T value, uint32_t size) {
   }
 
   buffer[index] = value;
+}
+
+/**
+ * Initialize a graph structure (graph_d) to be passed as a parameter to GPU 
+ * kernels. Both graph_d and graph_h structs reside in host memory. The 
+ * vertices, edges and weights pointers in graph_d will point to buffers in 
+ * device memory allocated by the routine. Also, the routine will copy-in the 
+ * data to the aforementioned three buffers from the corresponding buffers in 
+ * graph_h.
+ * @param[in] graph_h source graph which hosts references to main memory buffers
+ * @param[out] graph_d allocated graph that hosts references to device buffers
+ * @return generic success or failure
+ */
+inline error_t graph_initialize_device(graph_t* graph_h, graph_t** graph_d) {
+  assert(graph_h);
+
+  // Allocate the graph struct that will host references to device buffers
+  *graph_d = (graph_t*)malloc(sizeof(graph_t));
+  if (*graph_d == NULL) return FAILURE;
+
+  /* Copy basic data types within the structure, the buffers pointers will be 
+     overwritten next with device pointers */
+  **graph_d = *graph_h;
+
+  /* Allocate vertices, edges and weights device buffers and move them to 
+     the device. */
+  CHECK_ERR(cudaMalloc((void**)&(*graph_d)->vertices, 
+                       (graph_h->vertex_count + 1) * 
+                       sizeof(id_t)) == cudaSuccess, err);
+  CHECK_ERR(cudaMalloc((void**)&(*graph_d)->edges, graph_h->edge_count * 
+                       sizeof(id_t)) == cudaSuccess, err_free_vertices);
+  if (graph_h->weighted) {
+    CHECK_ERR(cudaMalloc((void**)&(*graph_d)->weights, graph_h->edge_count * 
+                         sizeof(weight_t)) == cudaSuccess, err_free_edges);
+  }
+
+  CHECK_ERR(cudaMemcpy((*graph_d)->vertices, graph_h->vertices, 
+                       (graph_h->vertex_count + 1) * sizeof(id_t), 
+                       cudaMemcpyHostToDevice) == cudaSuccess, 
+            err_free_weights);
+  CHECK_ERR(cudaMemcpy((*graph_d)->edges, graph_h->edges, 
+                       graph_h->edge_count * sizeof(id_t),
+                       cudaMemcpyHostToDevice) == cudaSuccess, 
+            err_free_weights);
+  if (graph_h->weighted) {
+  CHECK_ERR(cudaMemcpy((*graph_d)->weights, graph_h->weights, 
+                       graph_h->edge_count * sizeof(weight_t),
+                       cudaMemcpyHostToDevice) == cudaSuccess, 
+            err_free_weights);
+  }
+
+  return SUCCESS;
+
+ err_free_weights:
+  if ((*graph_d)->weighted) cudaFree((*graph_d)->weights);
+ err_free_edges:
+  cudaFree((*graph_d)->edges);
+ err_free_vertices:
+  cudaFree((*graph_d)->vertices);
+ err:
+  free(*graph_d);
+  printf("%d\n", cudaGetLastError());
+  return FAILURE;
+}
+
+/**
+ * Free allocated device buffers associated with the graph
+ * @param[in] graph_d the graph to be finalized
+ */
+inline void graph_finalize_device(graph_t* graph_d) {
+  assert(graph_d);
+  cudaFree(graph_d->edges);
+  cudaFree(graph_d->vertices);
+  if (graph_d->weighted) cudaFree(graph_d->weights);
+  free(graph_d);
 }
 
 #endif  // TOTEM_COMKERNEL_CUH
