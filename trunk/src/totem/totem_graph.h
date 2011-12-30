2510cf1f3600fd9ade3cc694f62db0d6b24ba8f7
ahgharaibeh <ahgharaibeh@b13f6db2-a700-491f-84d0-85bdae213c3a>
2011-12-30 02:57:56 +0000

    Create partitions interface. Review at http://codereview-netsyslab.appspot.com/210002/

--- trunk/src/totem/totem_graph.h
+++ trunk/src/totem/totem_graph.h
@@ -68,7 +68,7 @@ const id_t INFINITE = UINT32_MAX;
 typedef float weight_t;
 
 /**
- * Specifies the maximum value a weight can hold .
+ * Specifies the maximum value a weight can hold.
  */
 const weight_t WEIGHT_MAX = FLT_MAX;
 
@@ -81,6 +81,48 @@ const weight_t DEFAULT_EDGE_WEIGHT =  1;
  * Specifies the default vertex value in the vertex list
  */
 const weight_t DEFAULT_VERTEX_VALUE = 0;
+
+
+/**
+ * Log (base 2) of the maximum number of partitions. Practically, it specifies
+ * the number of bits allocated for the partition identifier when encoded in the
+ * vertex identifier in a partition's edges array
+ */
+#define MAX_LOG_PARTITION_COUNT  2
+
+/**
+ * Maximum number of partitions supported per graph
+ */
+#define MAX_PARTITION_COUNT (1 << (MAX_LOG_PARTITION_COUNT))
+
+/**
+ * Log (base 2) of the maximum number of vertices in a partition.
+ */
+#define MAX_LOG_VERTEX_COUNT     ((sizeof(id_t) * 8) - MAX_LOG_PARTITION_COUNT)
+
+/**
+ * A mask used to identify the vertex id bits by clearing out the partition id 
+ * bits which are assumed to be in the higher order bits
+ */
+#define VERTEX_ID_MASK           (((id_t)-1) >> MAX_LOG_PARTITION_COUNT)
+
+/**
+ * Decodes the partition id, which are placed in the higher order bits
+ */
+#define GET_PARTITION_ID(_vid)   ((_vid) >> (MAX_LOG_VERTEX_COUNT))
+
+/**
+ * Decodes the vertex id, which are placed in the lower order bits
+ */
+#define GET_VERTEX_ID(_vid)      ((_vid) & VERTEX_ID_MASK)
+
+/**
+ * Returns a new vertex id which encodes the correponding partition id in the
+ * higher order bits.
+ */
+#define SET_PARTITION_ID(_vid, _pid) \
+  ((_vid) | ((_pid) << MAX_LOG_VERTEX_COUNT))
+
 
 /**
  * A graph type based on adjacency list representation.
@@ -110,19 +152,43 @@ typedef struct graph_s {
   bool         directed;        /**< indicates if the graph is directed. */
 } graph_t;
 
+
+/**
+ * A graph partition type based on adjacency list representation. The vertex ids
+ * in the edges list have the partition id encoded in the most significant bits.
+ * This allows for a vertex to have a neighbor in another partition.
+ */
+typedef struct partition_s {
+  id_t*        vertices;        /**< the vertices list. */
+  id_t*        edges;           /**< the edges list. */
+  weight_t*    weights;         /**< stores the weights of the edges. */
+  uint64_t     vertex_count;    /**< number of vertices. */
+  uint64_t     edge_count;      /**< number of edges. */
+} partition_t;
+
+/**
+ * Defines a set of partitions
+ */
+typedef struct partition_set_s {
+  graph_t*     graph;           /**< the graph this partition set belongs to */
+  bool         weighted;        /**< indicates if edges have weights. */
+  partition_t* partitions;      /**< the partitions list */
+  int          partition_count; /**< number of partitions in the set */
+} partition_set_t;
+
 /**
  * Defines a data type for a graph's connected components. components are 
  * identified by numbers [0 - count). The marker array identifies for each
  * vertex the id of the component the vertex is part of.
  */
 typedef struct component_set_s {
-  graph_t* graph;        //**< the graph which this component set belongs to */
-  id_t     count;        //**< number of components */
-  id_t*    vertex_count; //**< vertex count of each component (length: count) */
-  id_t*    edge_count;   //**< edge count of each component (length: count) */
-  id_t*    marker;       //**< the component id for each vertex
-                         //**< (length: graph->vertex_count) */
-  id_t     biggest;      //**< the id of the biggest component */
+  graph_t* graph;        /**< the graph which this component set belongs to */
+  id_t     count;        /**< number of components */
+  id_t*    vertex_count; /**< vertex count of each component (length: count) */
+  id_t*    edge_count;   /**< edge count of each component (length: count) */
+  id_t*    marker;       /**< the component id for each vertex */
+                         /**< (length: graph->vertex_count) */
+  id_t     biggest;      /**< the id of the biggest component */
 } component_set_t;
 
 
@@ -194,6 +260,27 @@ graph_t* graph_create_bidirectional(graph_t* graph, id_t** reverse_indices);
  */
 error_t graph_random_partition(graph_t* graph, int number_of_partitions,
                                unsigned int seed, id_t** partition_labels);
+
+/**
+ * Creates the a partition set based on the vertex to partition assignment 
+ * specified in the lables array
+ *
+ * @param[in] graph the input graph
+ * @param[in] labels an array with a partition id for each vertex as
+ *                   identified by the array position
+ * @param[in] partition_count the number of partitions
+ * @param[out] partition_set the set of resulting graphs
+ * @return SUCCESS if the partitions are assigned, FAILURE otherwise.
+ */
+error_t graph_get_partitions(graph_t* graph, id_t* labels, int partition_count, 
+                             partition_set_t** partition_set);
+
+/**
+ * De-allocates a partition set object
+ * @param[in] partition_set a reference to partition set type to be de-allocated
+ * @return generic success or failure
+ */
+error_t finalize_partition_set(partition_set_t* partition_set);
 
 /**
  * Given an undirected, unweighted graph and a source vertex, find the minimum
