763b9f78a7367b1fff0440d203c311bef8f43097
Abdullah Gharaibeh <ahgharaibeh@users.noreply.github.com>
2012-01-06 19:37:18 +0000

    added more tests to the partition unit tests

--- src/test/totem_partition_unittest.cc
+++ src/test/totem_partition_unittest.cc
@@ -21,12 +21,30 @@ class GraphPartitionTest : public ::testing::Test {
     partition_set_ = NULL;
   }
   virtual void TearDown() {
-    EXPECT_EQ(SUCCESS,  graph_finalize(graph));
+     graph_finalize(graph);
     if (partitions_ != NULL) {
       free(partitions_);
     }
     if (partition_set_ != NULL) {
-      partition_set_finalize(partition_set_);
+      EXPECT_EQ(SUCCESS, partition_set_finalize(partition_set_));
+    }
+  }
+  void TestGraph(graph_t* graph) {
+    EXPECT_EQ(SUCCESS, partition_random(graph, 3, 13, &partitions_));
+    EXPECT_EQ(SUCCESS, partition_set_initialize(graph, partitions_, 3, 
+                                                &partition_set_));
+    for (int pid = 0; pid < partition_set_->partition_count; pid++) {
+      partition_t* partition = &partition_set_->partitions[pid];
+      for (id_t vid = 0; vid < partition->vertex_count; vid++) {
+        for (id_t i = partition->vertices[vid]; 
+             i < partition->vertices[vid + 1]; i++) {
+          int nbr_pid = GET_PARTITION_ID(partition->edges[i]);
+          EXPECT_TRUE((nbr_pid < partition_set_->partition_count));
+          partition_t* nbr_partition = &partition_set_->partitions[nbr_pid];
+          id_t nbr_id = GET_VERTEX_ID(partition->edges[i]);
+          EXPECT_TRUE((nbr_id < nbr_partition->vertex_count));
+        }
+      }
     }
   }
 };
@@ -67,24 +85,19 @@ TEST_F(GraphPartitionTest , GetPartitionsSingleNodeGraph) {
 }
 
 TEST_F(GraphPartitionTest, GetPartitionsChainGraph) {
-  EXPECT_EQ(SUCCESS, graph_initialize(DATA_FOLDER("chain_1000_nodes.totem"),
-                                      false, &graph));
-  EXPECT_EQ(SUCCESS, partition_random(graph, 3, 13, &partitions_));
-  EXPECT_EQ(SUCCESS, partition_set_initialize(graph, partitions_, 3, 
-                                          &partition_set_));
-  for (int pid = 0; pid < partition_set_->partition_count; pid++) {
-    partition_t* partition = &partition_set_->partitions[pid];
-    for (id_t vid = 0; vid < partition->vertex_count; vid++) {
-      for (id_t i = partition->vertices[vid]; 
-           i < partition->vertices[vid + 1]; i++) {
-        int nbr_pid = GET_PARTITION_ID(partition->edges[i]);
-        EXPECT_TRUE((nbr_pid < partition_set_->partition_count));
-        partition_t* nbr_partition = &partition_set_->partitions[nbr_pid];
-        id_t nbr_id = GET_VERTEX_ID(partition->edges[i]);
-        EXPECT_TRUE((nbr_id < nbr_partition->vertex_count));
-      }
-    }
-  }
+  graph_initialize(DATA_FOLDER("chain_1000_nodes.totem"), false, &graph);
+  TestGraph(graph);
+}
+
+TEST_F(GraphPartitionTest, GetPartitionsStarGraph) {
+  graph_initialize(DATA_FOLDER("star_1000_nodes.totem"), false, &graph);
+  TestGraph(graph);
+}
+
+TEST_F(GraphPartitionTest, GetPartitionsCompleteGraph) {
+    graph_initialize(DATA_FOLDER("complete_graph_300_nodes.totem"), 
+                     false, &graph);
+  TestGraph(graph);
 }
 
 TEST_F(GraphPartitionTest, GetPartitionsImbalancedChainGraph) {
@@ -108,33 +121,4 @@ TEST_F(GraphPartitionTest, GetPartitionsImbalancedChainGraph) {
                 partition->vertices[vid + 1] - partition->vertices[vid]);
     }
   }
-}
-
-TEST_F(GraphPartitionTest, GetPartitionZeroModularity) {
-  EXPECT_EQ(SUCCESS, graph_initialize(DATA_FOLDER("single_node.totem"),
-                                      false, &graph));
-  id_t* partition_labels = (id_t*)calloc(1, sizeof(id_t));
-  double modularity = -1;
-  EXPECT_EQ(SUCCESS, partition_set_initialize(graph, partition_labels, 1,
-                     &partition_set_));
-  EXPECT_EQ(SUCCESS,
-            partition_modularity(graph, partition_set_, &modularity));
-  EXPECT_EQ(0.0, modularity);
-  free(partition_labels);
-}
-
-TEST_F(GraphPartitionTest, GetPartitionModularity) {
-  EXPECT_EQ(SUCCESS, graph_initialize(DATA_FOLDER("chain_1000_nodes.totem"),
-                                      false, &graph));
-  // Divide the in two partitions -- one node in one partition and the other 999
-  // in the second partition.
-  partitions_ = (id_t*)calloc(1000, sizeof(id_t));
-  partitions_[0] = 1;
-  EXPECT_EQ(SUCCESS, partition_set_initialize(graph, partitions_, 2,
-                     &partition_set_));
-
-  double modularity = -1;
-  EXPECT_EQ(SUCCESS,
-            partition_modularity(graph, partition_set_, &modularity));
-  EXPECT_NEAR(0.99899, modularity, 0.00001);
 }
