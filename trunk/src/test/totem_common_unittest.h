385defd6e504c03203b9ef8270aca3272bcbc0cd
ahgharaibeh <ahgharaibeh@b13f6db2-a700-491f-84d0-85bdae213c3a>
2012-02-09 21:29:21 +0000

    move the KERNEL_EXPECT_TRUE macro to the common header file

--- trunk/src/test/totem_common_unittest.h
+++ trunk/src/test/totem_common_unittest.h
@@ -29,4 +29,21 @@
     }                                      \
   } while (0)
 
+/**
+ * A simple macro to do basic true/false condition testing for kernels
+ * TODO(abdullah): change the way state is tested to use standard report from 
+ * the GTest framework as follows:
+ * 1. to use the macro to test (so the code will be simple).
+ * 2. still have the variable to store the line number where it fails or -1
+ * otherwise.
+ * 3. in the test fixture you would copy back the variable with the line number
+ * and expects -1.
+ */
+#define KERNEL_EXPECT_TRUE(stmt)                \
+  do {                                          \
+    if (!(stmt)) {                              \
+      printf("Error line: %d\n", __LINE__);     \
+    }                                           \
+  } while(0)
+
 #endif // TOTEM_COMMON_UNITTEST_H
