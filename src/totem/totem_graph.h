47630b268b1e3f9027e6c5e17b1e20131a101e06
redekopg <redekopg@b13f6db2-a700-491f-84d0-85bdae213c3a>
2012-06-12 22:59:14 +0000

    GPU Betweenness Centrality (predecessor version), extracting SSSP & dependence. Review at http://codereview-netsyslab.appspot.com/359001/.

--- src/totem/totem_graph.h
+++ src/totem/totem_graph.h
@@ -380,7 +380,8 @@ error_t node_degree_gpu(const graph_t* graph, id_t** node_degree);
 error_t get_components_cpu(graph_t* graph, component_set_t** comp_set_ret);
 
 /**
- * Calculate betweenness centrality scores for unweighted graphs
+ * Calculate betweenness centrality scores for unweighted graphs using the
+ * successors stack implementation.
  * @param[in] graph the graph to use
  * @param[out] centrality_score the output list of betweenness centrality scores
  *             per vertex
@@ -388,6 +389,19 @@ error_t get_components_cpu(graph_t* graph, component_set_t** comp_set_ret);
  */
 error_t betweenness_unweighted_cpu(const graph_t* graph,
                                    weight_t** centrality_score);
+error_t betweenness_unweighted_gpu(const graph_t* graph,
+                                   weight_t** centrality_score);
+
+/**
+ * Calculate betweenness centrality scores for unweighted graphs using the
+ * predecessors map implementation.
+ * @param[in] graph the graph to use
+ * @param[out] centrality_score the output list of betweenness centrality scores
+ *             per vertex
+ * @return generic success or failure
+ */
+error_t betweenness_unweighted_shi_gpu(const graph_t* graph,
+                                       weight_t** centrality_score);
 
 /**
  * De-allocates a component_set_t object
