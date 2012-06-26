5518f51bb1c41569582aeb104001a3d5c589748b
redekopg <redekopg@b13f6db2-a700-491f-84d0-85bdae213c3a>
2012-06-26 21:22:04 +0000

    Remove r_edges from CPU closeness centrality. Review at: http://codereview-netsyslab.appspot.com/379001/

--- src/totem/totem_closeness.cu
+++ src/totem/totem_closeness.cu
@@ -103,7 +103,7 @@ error_t closeness_apsp_gpu(const graph_t* graph, id_t source, graph_t* graph_d,
     CHK_CU_SUCCESS(cudaDeviceSynchronize(), err);
     CHK_CU_SUCCESS(cudaMemcpy(&finished, finished_d, sizeof(bool),
                               cudaMemcpyDeviceToHost), err);
-    *dist++;
+    (*dist)++;
   }}
   return SUCCESS;
 
@@ -214,63 +214,43 @@ error_t closeness_unweighted_cpu(const graph_t* graph,
   // Allocate space for the results
   weight_t* closeness_centrality =
     (weight_t*)mem_alloc(graph->vertex_count * sizeof(weight_t));
-  // Construct the reverse edges list (graph->edges is a list of destination
-  // vertices, r_edges is a list of source vertices, indexed by edge id)
-  id_t* r_edges = NULL;
-  centrality_construct_r_edges(graph, &r_edges);
 
   // Allocate and initialize state for the problem
-  int32_t* dists = (int32_t*)mem_alloc(graph->vertex_count * sizeof(int32_t));
+  uint32_t* dists =
+    (uint32_t*)mem_alloc(graph->vertex_count * sizeof(uint32_t));
   memset(closeness_centrality, 0.0, graph->vertex_count * sizeof(weight_t));
 
   for (id_t source = 0; source < graph->vertex_count; source++) {
     // Initialize state for SSSP
-    memset(dists, -1, graph->vertex_count * sizeof(int32_t));
+    memset(dists, -1, graph->vertex_count * sizeof(uint32_t));
     dists[source] = 0;
 
     // SSSP and path counting
-    int32_t dist = 0;
+    uint32_t dist = 0;
     bool finished = false;
     while (!finished) {
       finished = true;
       #ifdef _OPENMP
       #pragma omp parallel for
       #endif // _OPENMP
-      for (id_t e = 0; e < graph->edge_count; e++) {
-        id_t e_source = r_edges[e];
-        id_t e_dest = graph->edges[e];
-
-        if ((dists[e_source] == dist) && (dists[e_dest] == -1)) {
-          finished = false;
-          dists[e_dest] = dist + 1;
+      for (id_t vertex_id = 0; vertex_id < graph->vertex_count; vertex_id++) {
+        if (dists[vertex_id] != dist) continue;
+        for (id_t i = graph->vertices[vertex_id];
+             i < graph->vertices[vertex_id + 1]; i++) {
+          const id_t neighbor_id = graph->edges[i];
+          if (dists[neighbor_id] == (uint32_t)-1) {
+            finished = false;
+            dists[neighbor_id] = dist + 1;
+          }
         }
       }
       dist++;
     }
     // Count connected and calculate closeness centrality
-    uint32_t connected = graph->vertex_count;
-    uint64_t sum = 0;
-    for (id_t v = 0; v < graph->vertex_count; v++) {
-      if (dists[v] == -1) {
-        connected--;
-      } else {
-        sum += dists[v];
-      }
-    }
-
-    // If this node is isolated or has a 0 length path to all other nodes, set
-    // its centrality explicitly to 0
-    if ((connected == 0) || (sum == 0)) {
-      closeness_centrality[source] = (weight_t)0.0;
-    } else {
-      closeness_centrality[source] =
-        (weight_t)(1.0 * (connected - 1) * (connected - 1))
-          / ((graph->vertex_count - 1) * sum);
-    }
+    calculate_closeness(graph, source, dists, closeness_centrality);
   } // for
 
   // Cleanup phase
-  mem_free(r_edges);
   mem_free(dists);
 
   // Return the centrality
