7baf1f93c8523aa5557ca1f965c2c4f201de82e8
Abdullah Gharaibeh <ahgharaibeh@users.noreply.github.com>
2013-02-16 17:46:33 +0000

    Added a script to generate scale-free networks using BTER graph generator. Review at: http://codereview-netsyslab.appspot.com/557001/

---
+++ src/scripts/bter/scale-free.m
@@ -0,0 +1,49 @@
+#! /usr/bin/octave -qf
+
+% Creates a scale-free network using bter random graph generator.
+%
+% The graph is saved as an octave spare matrix, which has the following format:
+%  # name: G
+%  # type: sparse matrix
+%  # nnz: <number of edges>
+%  # rows: <number of vertices>
+%  # columns: <number of vertices>
+%  <source node id> <destination node id> <connected>
+%
+% Example:
+% ./scale-free.m 1 3 /tmp/test.mat; cat /tmp/test.mat
+% # Created by Octave 3.4.3
+% # name: G
+% # type: sparse matrix
+% # nnz: 8
+% # rows: 6
+% # columns: 6
+% 3 1 1
+% 4 2 1
+% 1 3 1
+% 2 4 1
+% 5 4 1
+% 4 5 1
+% 6 5 1
+% 5 6 1
+%
+% Created on: 2013-02-16
+% Author: Abdullah Gharaibeh
+
+% Get command line arguments
+arg_list = argv();
+if length(arg_list) != 3
+  fprintf("\nError: missing arguments\n");
+  fprintf("usage: %s <alpha> <maxdeg> <output_file>\n", program_name());
+  exit(-1)
+end
+alpha = str2double(arg_list{1});
+maxdeg = str2num(arg_list{2});
+saveat = arg_list{2};
+
+% Create a power-law distribution of edge degree
+ddist = round(maxdeg^(alpha)./(1:maxdeg).^(alpha))';
+
+% Invoke BTER generator and save the generated sparse matrix
+G = bter(ddist);
+save(saveat, "G");
