a966340fb3566c324cac118b75c2db1a64598ef7
sidneyp <sidneyp@b13f6db2-a700-491f-84d0-85bdae213c3a>
2013-06-03 19:00:58 +0000

    Modified files and Header File for intensive benchmarks

--- trunk/src/util/totem_cpu_intensive.cc
+++ trunk/src/util/totem_cpu_intensive.cc
@@ -5,58 +5,35 @@
  * Author: Sidney Pontes Filho
  */
 
-// system includes
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 // totem includes
-#include "totem_comdef.h"
+#include "totem_intensive_benchmark.h"
 
 /**
- * Defines the duration of the running time when user does not
- * pass any argument.
- */
-#define DURATION_DEFAULT 1.0
-
-// Help message is displayed when user passes a wrong argument.
-void help_message() { 
-  printf("Usage: ./[EXECUTABLE]\n");
-  printf("  or:  ./[EXECUTABLE] [NUMBER]\n");
-  printf("  or:  ./[EXECUTABLE] [NUMBER].[NUMBER]\n");
-} 
-
-/**
- * A cpu-intensive routine that computes products and subtractions
- * of a elements in an array in parallel using OMP. 
+ * A cpu-intensive routine that computes some mathematical operations
+ * in parallel using OMP.
  * @param[in] argv[1] duration of the running time in seconds
  */
 int main (int argc, char *argv[]) {
-  double a = 1.0;
-  double duration;
   time_t start, end;  
   time(&start);
   if (argc > 2) {
     help_message();
     exit(EXIT_FAILURE);
-  } else {
-    duration = (argc == 2) ? atof(argv[1]) : DURATION_DEFAULT;
-    if (duration == 0.0f) { 
-      help_message();
-      exit(EXIT_FAILURE);
-    }    
-    OMP(omp parallel for)
-    for (long x = 0; x < 1000; x++) {
-      a = (double)x - a * 0.00001;
-      x = x - a * a;      
+  } 
+  double duration = (argc == 2) ? atof(argv[1]) : DURATION_DEFAULT;
+  if (duration == 0.0f) { 
+    help_message();
+    exit(EXIT_FAILURE);
+  }    
+  OMP(omp parallel)
+  {
+    double a, b;
+    do {
+      a = drand48();
+      b = drand48() * DBL_MAX;
+      b = (a * b) - (a / b) + (pow(a, b));
       time(&end);
-
-      // Stop condition. 
-      // OpenMP does not allow 'break' inside a parallel region.
-      if (difftime(end, start) >= duration) {
-        x = 10000000;
-      }
-    }
-  }  
+    } while (difftime(end, start) < duration);
+  }
   return 0;
 }
