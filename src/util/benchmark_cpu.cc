4dc7ffaf05f45f3e15cd981d17004f1e8abe9582
sidneyp <sidneyp@b13f6db2-a700-491f-84d0-85bdae213c3a>
2013-07-19 20:37:16 +0000

    GPU-based intensive benchmark for power measurement experiments. Review at: https://codereview-netsyslab.appspot.com/741002/

--- src/util/benchmark_cpu.cc
+++ src/util/benchmark_cpu.cc
@@ -154,6 +154,7 @@ error_t memory_copy_intensive_cpu(double duration) {
   OMP(omp parallel for)
   for (uint64_t i = 0; i < SMALL_ARRAY_SIZE; i++) {
     ar1[i] = i;
+    ar2[i] = 0;
   }
   OMP(omp parallel)
   {
@@ -179,6 +180,72 @@ error_t memory_copy_intensive_cpu(double duration) {
       // Copy from ar2 to ar1
       memcpy(&ar1[piece_array_index], &ar2[piece_array_index],
              piece_array_size * sizeof(uint64_t));
+      OMP(omp barrier)
+      time(&end);
+    } while (difftime(end, start) < duration);
+  }
+  free(ar1);
+  free(ar2);
+  return SUCCESS;
+}
+
+/**
+ * A random memory copy routine between two arrays in parallel using OMP.
+ */
+error_t memory_random_copy_intensive_cpu(double duration) {
+  printf("Memory Copy Intensive Benchmark\n");
+  time_t start, end;
+  time(&start);
+  uint64_t *ar1 = (uint64_t *)malloc(SMALL_ARRAY_SIZE * sizeof(uint64_t));
+  uint64_t *ar2 = (uint64_t *)malloc(SMALL_ARRAY_SIZE * sizeof(uint64_t));
+
+  // Initialize the array elements with its index number.
+  OMP(omp parallel for)
+    for (uint64_t i = 0; i < SMALL_ARRAY_SIZE; i++) {
+      ar1[i] = i;
+      ar2[i] = 0;
+    }
+  OMP(omp parallel)
+  {
+    uint64_t nthreads = omp_get_num_threads();
+    uint64_t th_id = omp_get_thread_num();
+    
+    // Calculates the size of the piece of array for thread in charge
+    uint64_t piece_array_size = (SMALL_ARRAY_SIZE / nthreads)
+      + (((SMALL_ARRAY_SIZE % nthreads) > th_id) ? 1 : 0);
+    
+    // Calculates the beginning index of the piece of array for thread
+    // in charge
+    uint64_t piece_array_index = th_id * (SMALL_ARRAY_SIZE / nthreads)
+      + (((SMALL_ARRAY_SIZE % nthreads) > th_id)
+         ? th_id : (SMALL_ARRAY_SIZE % nthreads));
+    do {
+      uint64_t th_id_random = random_uint64(nthreads);
+      
+      // Calculates the size of the piece of array for a random position
+      uint64_t piece_array_size_random = (SMALL_ARRAY_SIZE / nthreads)
+        + (((SMALL_ARRAY_SIZE % nthreads) > th_id_random) ? 1 : 0);
+      
+      // Calculates the beginning index of the piece of array for a random
+      // position
+      uint64_t piece_array_index_random = th_id_random 
+        * (SMALL_ARRAY_SIZE / nthreads) 
+        + (((SMALL_ARRAY_SIZE % nthreads) > th_id_random)
+           ? th_id_random : (SMALL_ARRAY_SIZE % nthreads));
+
+      // Copy from ar1 to ar2
+      memcpy(&ar2[piece_array_index_random], &ar1[piece_array_index],
+             ((piece_array_size > piece_array_size_random) 
+              ? piece_array_size_random : piece_array_size) 
+             * sizeof(uint64_t));
+      OMP(omp barrier)
+          
+      // Copy from ar2 to ar1
+      memcpy(&ar1[piece_array_index_random], &ar2[piece_array_index],
+             ((piece_array_size > piece_array_size_random)
+              ? piece_array_size_random : piece_array_size)
+             * sizeof(uint64_t));
+
       OMP(omp barrier)
       time(&end);
     } while (difftime(end, start) < duration);
