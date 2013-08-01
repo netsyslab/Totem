c7d765912cce0344aaf0466c6b2e1933517cd565
ahgharaibeh <ahgharaibeh@b13f6db2-a700-491f-84d0-85bdae213c3a>
2013-08-01 07:24:21 +0000

    Modified the BFS algorithm to build a frontier bitmap. This new bitmap allows to tune the CPU kernel to quickly iterate over the vertices when the frontier is sparse. Also, for both the CPU and GPU kernels, it allows them to completely skip iterating over all the vertices when the forntier length is zero. Review at: https://codereview-netsyslab.appspot.com/761002/

--- trunk/src/totem/totem_bitmap.cuh
+++ trunk/src/totem/totem_bitmap.cuh
@@ -32,6 +32,10 @@ __device__ __host__ inline size_t bitmap_bits_to_bytes(vid_t len) {
   return ((len / BITMAP_BITS_PER_WORD + 1) * sizeof(bitmap_word_t));
 }
 
+__device__ __host__ inline size_t bitmap_bits_to_words(vid_t len) {
+  return (len / BITMAP_BITS_PER_WORD + 1);
+}
+
 /**
  * Allocates and initializes a bitmap data structure
  * @param[in]  len  the length of the bitmap
@@ -119,5 +123,32 @@ __device__ inline bool bitmap_unset_gpu(bitmap_t map, vid_t bit) {
 __host__ __device__ inline bool bitmap_is_set(bitmap_t map, vid_t bit) {
   return (map[bit / BITMAP_BITS_PER_WORD] & bitmap_bit_mask(bit));
 }
+
+/**
+ * A multi-purpose function that is motivated by traversal-based algorithms
+ * to create the frontier bitmap and get the number of vertices in the frontier.
+ * This function does three things: first, cur and diff are xor-ed and the 
+ * result is stored back in diff (i.e., diffing cur and the old value in diff);
+ * second, count the number of set bits in diff after the diffing; finally,
+ * copy cur bitmap to "copy".
+ * @param[in] cur the current visited state bitmap
+ * @param[in/out] diff at entry, represents the bitmap of last visited round, 
+ *                when the function returns, it will represent the diff 
+ *                between the current and last visited bitmaps (i.e., the
+ *                frontier).
+ * @param[out] copy used to backup the cur bitmap
+ * @param[in] len the length of the bitmaps
+ * @param[in] count_d a reference to a sizeof(vid_t) worth of space
+ *                    on device memory used to compute the number
+ *                    of set bits in the diff bitmap.
+ * @param[in] stream the stream within which this computation will be launched
+ *
+ * @return the number of set bits in diff
+*/
+vid_t bitmap_diff_copy_count_gpu(bitmap_t cur, bitmap_t diff, bitmap_t copy,
+                                 size_t len, vid_t* count_d, 
+                                 cudaStream_t stream);
+vid_t bitmap_diff_copy_count_cpu(bitmap_t cur, bitmap_t diff, bitmap_t copy,
+                                 size_t len);
 
 #endif // TOTEM_BITMAP_H
