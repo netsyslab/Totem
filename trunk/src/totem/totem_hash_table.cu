757950ec750c6c60b09e3f935d426a4c51a0c1b3
ahgharaibeh <ahgharaibeh@b13f6db2-a700-491f-84d0-85bdae213c3a>
2013-08-16 22:24:19 +0000

    Increased the size of the space pre-allocated for hash tables

--- trunk/src/totem/totem_hash_table.cu
+++ trunk/src/totem/totem_hash_table.cu
@@ -23,7 +23,7 @@
  * The ratio of the space to be allocated in the table with respect to the
  * maximum number of entries to be hosted in the table.
  */
-#define HT_SPACE_EXPANSION_RATIO 2.5
+#define HT_SPACE_EXPANSION_RATIO 3
 
 /**
  * Maximum length of the eviction (collision) chain during the key
