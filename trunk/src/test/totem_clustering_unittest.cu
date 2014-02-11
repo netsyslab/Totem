f8d102848472f46df267e79a2ddd4344c880a07b
treza <treza@b13f6db2-a700-491f-84d0-85bdae213c3a>
2014-02-11 04:52:33 +0000

    Added new tests for the Clustering Coefficient algorithm, CPU and GPU-only.

--- trunk/src/test/totem_clustering_unittest.cu
+++ trunk/src/test/totem_clustering_unittest.cu
@@ -1,8 +1,12 @@
 /*
- * Contains unit tests for local clustering coefficient.
+ * Unit tests for clustering coefficient algorithm.
  *
  * Created on: 2013-07-09
  * Author: Sidney Pontes Filho
+ *
+ * Last updated on: 2014-02-03
+ * Author: Tahsin Arafat Reza 
+ *
  */
 
 // totem includes
@@ -15,10 +19,10 @@ using ::testing::Values;
 
 // The following implementation relies on
 // TestWithParam<ClusteringCoefficientFunction> to test the two versions of 
-// Clustering Coefficient implemented: CPU.  Details on how to use 
+// Clustering Coefficient implemented for: CPU and GPU.  Details on how to use 
 // TestWithParam<T> can be found at:
 // http:
-//code.google.com/p/googletest/source/browse/trunk/samples/sample7_unittest.cc
+// code.google.com/p/googletest/source/browse/trunk/samples/sample7_unittest.cc
 
 typedef error_t(*ClusteringCoefficientFunction)(const graph_t*, weight_t**);
 
@@ -27,71 +31,134 @@ public TestWithParam<ClusteringCoefficientFunction> {
  public:
   virtual void SetUp() {
     // Ensure the minimum CUDA architecture is supported
-    // CUDA_CHECK_VERSION();
+    CUDA_CHECK_VERSION();
     clustering = GetParam();
+    _graph = NULL; 
+    _coefficients = NULL;
+    _mem_type = TOTEM_MEM_HOST_PINNED;		
+  }
+  
+  virtual void TearDown() {
+    if(_graph) graph_finalize(_graph);
+    if(_coefficients) totem_free(_coefficients, _mem_type);
   }
 
  protected:
    ClusteringCoefficientFunction clustering;
-   graph_t* graph;
-   weight_t* coefficient_score;
+   graph_t* _graph;
+   weight_t* _coefficients;
+   totem_mem_t _mem_type;
 };
 
-
-// Tests ClusteringCoefficient for empty graphs.
-TEST_P(ClusteringCoefficientTest, Empty) {
-  graph_t empty_graph;
-  empty_graph.directed = false;
-  empty_graph.vertex_count = 0;
-  empty_graph.edge_count = 0;
-  EXPECT_EQ(FAILURE, clustering(&empty_graph, &coefficient_score));
+// Tests ClusteringCoefficient for an empty graph.
+TEST_P(ClusteringCoefficientTest, EmptyGraph) {
+  graph_t graph;
+  graph.directed = false;
+  graph.vertex_count = 0;
+  graph.edge_count = 0;
+  EXPECT_EQ(FAILURE, clustering(&graph, &_coefficients));
 }
 
-// Tests ClusteringCoefficient for single node graphs.
-TEST_P(ClusteringCoefficientTest, SingleNodeUnweighted) {
-  graph_t* graph;
+// Tests ClusteringCoefficient for a single node graph.
+TEST_P(ClusteringCoefficientTest, SingleNodeGrpah) {
   EXPECT_EQ(SUCCESS, graph_initialize(DATA_FOLDER("single_node.totem"),
-                                      false, &graph));
-
-  EXPECT_EQ(SUCCESS, clustering(graph, &coefficient_score));
-  EXPECT_FALSE(coefficient_score == NULL);
-  EXPECT_EQ((weight_t)0.0, coefficient_score[0]);
-  totem_free(coefficient_score, TOTEM_MEM_HOST_PINNED);
-  EXPECT_EQ(SUCCESS, graph_finalize(graph));
+                                      false, &_graph));
+  
+  CALL_SAFE(totem_malloc(_graph->vertex_count * sizeof(weight_t), _mem_type,
+                         (void**)&_coefficients)); 
+ 
+  EXPECT_EQ(SUCCESS, clustering(_graph, &_coefficients));
+  EXPECT_FALSE(_coefficients == NULL);
+  EXPECT_EQ((weight_t)0.0, _coefficients[0]);  
 }
 
-// Tests ClusteringCoefficient for a complete graph of 300 undirected nodes.
+// Tests ClusteringCoefficient for an undirected complete graph with 
+// 300 nodes.
 TEST_P(ClusteringCoefficientTest, CompleteGraph300NodesUndirected) {
   EXPECT_EQ(SUCCESS,
             graph_initialize(DATA_FOLDER("complete_graph_300_nodes.totem"),
-                             false, &graph));
-
-  EXPECT_EQ(SUCCESS, clustering(graph, &coefficient_score));
-  EXPECT_FALSE(coefficient_score == NULL);
-  for(vid_t vertex = 0; vertex < graph->vertex_count; vertex++){
-    EXPECT_FLOAT_EQ(1.0, coefficient_score[0]);
-  }
-  totem_free(coefficient_score, TOTEM_MEM_HOST_PINNED);
-  EXPECT_EQ(SUCCESS, graph_finalize(graph));
+                             false, &_graph));
+  
+  CALL_SAFE(totem_malloc(_graph->vertex_count * sizeof(weight_t), _mem_type,
+                         (void**)&_coefficients)); 
+   
+  EXPECT_EQ(SUCCESS, clustering(_graph, &_coefficients));
+  EXPECT_FALSE(_coefficients == NULL);
+  for(vid_t vertex = 0; vertex < _graph->vertex_count; vertex++){
+    EXPECT_FLOAT_EQ(1.0, _coefficients[vertex]);
+  }  
 }
 
-
-// Tests ClusteringCoefficient for a complete acyclic graph of 100 directed 
-// nodes.
-TEST_P(ClusteringCoefficientTest, CompleteAcyclicGraph100NodesDirected) {
+// Tests ClustreingCoefficinet for an undirected chain graph with 1K nodes.
+TEST_P(ClusteringCoefficientTest, ChainGraph1000NodesUndirected) {
   EXPECT_EQ(SUCCESS,
-            graph_initialize(DATA_FOLDER("acyclic_100_nodes.totem"),
-                             false, &graph));
+            graph_initialize(DATA_FOLDER("chain_1000_nodes.totem"),
+                             false, &_graph));
 
-  EXPECT_EQ(SUCCESS, clustering(graph, &coefficient_score));
-  EXPECT_FALSE(coefficient_score == NULL);
-  for(vid_t vertex = 0; vertex < graph->vertex_count; vertex++){
-    EXPECT_FLOAT_EQ(1.0, coefficient_score[0]);
-  }
-  totem_free(coefficient_score, TOTEM_MEM_HOST_PINNED);
-  EXPECT_EQ(SUCCESS, graph_finalize(graph));
+  CALL_SAFE(totem_malloc(_graph->vertex_count * sizeof(weight_t), _mem_type, 
+                         (void**)&_coefficients)); 
+  
+  EXPECT_EQ(SUCCESS, clustering(_graph, &_coefficients));
+  EXPECT_FALSE(_coefficients == NULL);
+  for(vid_t vertex = 0; vertex < _graph->vertex_count; vertex++){
+    EXPECT_FLOAT_EQ(0.0, _coefficients[vertex]);
+  }  
 }
 
+// Tests ClustreingCoefficinet for an undirected star graph with 1K nodes.
+TEST_P(ClusteringCoefficientTest, StarGraph1000NodesUndirected) {
+  EXPECT_EQ(SUCCESS,
+            graph_initialize(DATA_FOLDER("star_1000_nodes.totem"),
+                             false, &_graph));
+
+  CALL_SAFE(totem_malloc(_graph->vertex_count * sizeof(weight_t), _mem_type, 
+                         (void**)&_coefficients)); 
+  
+  EXPECT_EQ(SUCCESS, clustering(_graph, &_coefficients));
+  EXPECT_FALSE(_coefficients == NULL);
+  for(vid_t vertex = 0; vertex < _graph->vertex_count; vertex++){
+    EXPECT_FLOAT_EQ(0.0, _coefficients[vertex]);
+  }  
+}
+
+// Tests ClustreingCoefficinet for a graph with 1K disconnected nodes.
+TEST_P(ClusteringCoefficientTest, DisconnectedGraph1000Nodes) {
+  EXPECT_EQ(SUCCESS,
+            graph_initialize(DATA_FOLDER("disconnected_1000_nodes.totem"),
+                             false, &_graph));
+
+  CALL_SAFE(totem_malloc(_graph->vertex_count * sizeof(weight_t), _mem_type, 
+                         (void**)&_coefficients)); 
+  
+  EXPECT_EQ(SUCCESS, clustering(_graph, &_coefficients));
+  EXPECT_FALSE(_coefficients == NULL);
+  for(vid_t vertex = 0; vertex < _graph->vertex_count; vertex++){
+    EXPECT_FLOAT_EQ(0.0, _coefficients[vertex]);
+  }  
+}
+
+// Tests ClustreingCoefficinet for an undirected ring center graph with 1K 
+// nodes.
+TEST_P(ClusteringCoefficientTest, RingCenterGraph1000NodesUndirected) {
+  EXPECT_EQ(SUCCESS,
+            graph_initialize(DATA_FOLDER("ring_center_graph_1000_nodes.totem"),
+                             false, &_graph));
+
+  CALL_SAFE(totem_malloc(_graph->vertex_count * sizeof(weight_t), _mem_type, 
+                         (void**)&_coefficients)); 
+  
+  EXPECT_EQ(SUCCESS, clustering(_graph, &_coefficients));
+  EXPECT_FALSE(_coefficients == NULL);
+
+  weight_t expected_cc_center_vertex = 
+           2.0f / ((weight_t)_graph->vertex_count - 2.0f);
+  EXPECT_FLOAT_EQ(expected_cc_center_vertex, _coefficients[0]);
+
+  weight_t expected_cc_ring_vertex = 2.0f / 3.0f;
+  for(vid_t vertex = 1; vertex < _graph->vertex_count; vertex++){
+    EXPECT_FLOAT_EQ(expected_cc_ring_vertex, _coefficients[vertex]);
+  }  
+}
 
 // From Google documentation:
 // In order to run value-parameterized tests, we need to instantiate them,
@@ -100,8 +167,9 @@ TEST_P(ClusteringCoefficientTest, CompleteAcyclicGraph100NodesDirected) {
 // Values() receives a list of parameters and the framework will execute the
 // whole set of tests ClusteringCoefficientTest for each element of Values()
 INSTANTIATE_TEST_CASE_P(ClusteringCoefficientGPUandCPUTest, 
-                        ClusteringCoefficientTest,
-                        Values(&local_clustering_coefficient_cpu));
+                        ClusteringCoefficientTest, 
+                        Values(&clustering_coefficient_cpu,
+                        &clustering_coefficient_gpu));
 
 #else
 
