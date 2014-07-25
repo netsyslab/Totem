405e600c0f8805a3e0c75fa2276d9793f29a10e7
treza <treza@b13f6db2-a700-491f-84d0-85bdae213c3a>
2014-07-25 23:52:47 +0000

    Added TODO for future update of the clustering coefficient algorithm

--- trunk/src/alg/totem_alg.h
+++ trunk/src/alg/totem_alg.h
@@ -127,10 +127,17 @@ void graph500_alloc(partition_t* par);
  * @param[in] graph the input graph
  * @param[out] coefficients array containing computed coefficinets
  */
+ //TODO(treza): change the data type from weight_t to something specific 
+ //to clustering coefficient (e.g., clustering_coefficient_t) or 
+ //even change this to a template.
 error_t clustering_coefficient_cpu(const graph_t* graph, 
                                    weight_t** coefficients);
 error_t clustering_coefficient_gpu(const graph_t* graph, 
                                    weight_t** coefficients);
+error_t clustering_coefficient_sorted_neighbours_cpu(const graph_t* graph, 
+                                                     weight_t** coefficients);
+error_t clustering_coefficient_sorted_neighbours_gpu(const graph_t* graph, 
+                                                     weight_t** coefficients);
 
 /*
  * Given a weighted graph \f$G = (V, E, w)\f$ and a source vertex \f$v\inV\f$,
