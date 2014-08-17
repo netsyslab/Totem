659739845dd682127b4e57b97108d85e85214512
Abdullah Gharaibeh <ahgharaibeh@users.noreply.github.com>
2014-08-17 03:39:01 +0000

    Added a new macro to help time function calls. Review at:https://codereview-netsyslab.appspot.com/8674002/

--- src/totem/totem_comdef.h
+++ src/totem/totem_comdef.h
@@ -27,21 +27,16 @@
 // TBB includes
 #include "tbb/parallel_sort.h"
 
-
-/**
- *  Function return code types
- */
+// Function return code types
 const int SUCCESS = 0;
 const int FAILURE = -1;
 
-/**
- * Processor types. This is used as to identify the the processor on which
- * a partition is processed on.
- */
+// Processor types. This is used as to identify the the processor on which
+// a partition is processed on.
 typedef enum {
-  PROCESSOR_CPU = 0, /**< CPU processor */
-  PROCESSOR_GPU,     /**< GPU processor */
-  PROCESSOR_MAX      /**< Indicates the number of supported processor types */
+  PROCESSOR_CPU = 0,
+  PROCESSOR_GPU,
+  PROCESSOR_MAX
 } processor_type_t;
 
 typedef struct processor_s {
@@ -49,19 +44,14 @@ typedef struct processor_s {
   uint32_t         id;   /**< Used to id GPU devices */
 } processor_t;
 
-/**
- * Stopwatch (timer) type
- */
+
+// Stopwatch (timer) type.
 typedef double stopwatch_t;
 
-/**
- * Used to define private functions and variables
- */
+// Used to define private functions and variables.
 #define PRIVATE static
 
-/**
- * This is defined if -fopenmp flag is passed to the compiler
- */
+// This is defined if -fopenmp flag is passed to the compiler.
 #if defined(_OPENMP)
 #define OMP(x) _Pragma(#x)
 #include <omp.h>
@@ -72,55 +62,43 @@ static int omp_get_num_threads (void) {return 1;}
 static int omp_get_max_threads (void) {return 1;}
 #endif
 
-/**
- * Used for bit-based space calculations
- */
+// Used for bit-based space calculations.
 const size_t BITS_PER_BYTE  = 8;
 const size_t BYTES_PER_WORD = (sizeof(uint64_t));
 const size_t BITS_PER_WORD  = (BYTES_PER_WORD * BITS_PER_BYTE);
-inline size_t bits_to_bytes(size_t bits) { 
+inline size_t bits_to_bytes(size_t bits) {
   return (((bits / BITS_PER_WORD) + 1) * BYTES_PER_WORD);
 }
 
-/*
- * Commonly used communication message sizes
- */
+// Commonly used communication message sizes.
 const size_t MSG_SIZE_ZERO = 0;
 const size_t MSG_SIZE_BYTE = BITS_PER_BYTE;
 const size_t MSG_SIZE_WORD = sizeof(int) * BITS_PER_BYTE;
 
-/*
- * A global seed value
- */
+// A global seed value.
 const int GLOBAL_SEED = 1985;
 
-/**
- * A wrapper that asserts the success of totem function calls
- */
+// A wrapper that asserts the success of totem function calls.
 #define CALL_SAFE(func)                         \
   do {                                          \
     error_t err = func;                         \
     assert(err == SUCCESS);                     \
-  } while(0)
+  } while (0)
 
-/**
- * Simulates simple exceptions: if the statement is not correct, jump to
- * label, typically an error label where you could clean up before exit.
- */
+// Simulates simple exceptions: if the statement is not correct, jump to
+// label, typically an error label where you could clean up before exit.
 #define CHK(stmt, label)                        \
   do {                                          \
     if (!(stmt))                                \
       goto label;                               \
-  } while(0)
+  } while (0)
 
-/**
- * Check if return value of stmt is SUCCESS, jump to label if not.
- */
+// Check if return value of stmt is SUCCESS, jump to label if not.
 #define CHK_SUCCESS(stmt, label) CHK((stmt) == SUCCESS, label)
 
 /**
- * Converts the string to upper case
- * @param[in] str the string to change to upper case
+ * Converts a string to upper case.
+ * @param[in] str the string to be converted.
  */
 inline void to_upper(char* str) {
   assert(str);
@@ -155,14 +133,15 @@ inline bool is_numeric(char* str) {
  * @return old value stored at address
  */
 inline float __sync_fetch_and_add_float(float* address, float val) {
-  int* address_as_int = (int*)address;
+  int* address_as_int = reinterpret_cast<int*>(address);
   int old = *address_as_int, assumed;
   do {
     assumed = old;
-    float sum = (val + *((float*)&assumed));
-    old = __sync_val_compare_and_swap(address_as_int, assumed, *((int*)&sum));
+    float sum = val + *(reinterpret_cast<float*>(&assumed));
+    old = __sync_val_compare_and_swap(
+        address_as_int, assumed, *(reinterpret_cast<int*>(&sum)));
   } while (assumed != old);
-  return *((float *)&old);
+  return *(reinterpret_cast<float*>(&old));
 }
 
 /**
@@ -173,15 +152,15 @@ inline float __sync_fetch_and_add_float(float* address, float val) {
  * @return old value stored at address
  */
 inline double __sync_fetch_and_add_double(double* address, double val) {
-  int64_t* address_as_int64 = (int64_t*)address;
+  int64_t* address_as_int64 = reinterpret_cast<int64_t*>(address);
   int64_t old = *address_as_int64, assumed;
   do {
     assumed = old;
-    double sum = val + (*((double*)&assumed));
+    double sum = val + (*(reinterpret_cast<double*>(&assumed)));
     old = __sync_val_compare_and_swap(address_as_int64, assumed,
-                                      *((int64_t*)&sum));
+                                      *(reinterpret_cast<int64_t*>(&sum)));
   } while (assumed != old);
-  return *((double *)&old);
+  return *(reinterpret_cast<double*>(&old));
 }
 
 /**
@@ -209,15 +188,16 @@ inline int __sync_fetch_and_min(int* address, int val) {
  * @return old value stored at address
  */
 inline float __sync_fetch_and_min_float(float* address, float val) {
-  int* address_as_int = (int*)address;
+  int* address_as_int = reinterpret_cast<int*>(address);
   int old = *address_as_int, assumed;
   do {
     assumed = old;
-    float assumed_float = *((float*)&assumed);
+    float assumed_float = *(reinterpret_cast<float*>(&assumed));
     float min = (val < assumed_float) ? val : assumed_float;
-    old = __sync_val_compare_and_swap(address_as_int, assumed, *((int*)&min));
+    old = __sync_val_compare_and_swap(address_as_int, assumed,
+                                      *(reinterpret_cast<int*>(&min)));
   } while (assumed != old);
-  return *((float *)&old);
+  return *(reinterpret_cast<float*>(&old));
 }
 
 /**
@@ -228,16 +208,16 @@ inline float __sync_fetch_and_min_float(float* address, float val) {
  * @return old value stored at address
  */
 inline double __sync_fetch_and_min_double(double* address, double val) {
-  int64_t* address_as_int64 = (int64_t*)address;
+  int64_t* address_as_int64 = reinterpret_cast<int64_t*>(address);
   int64_t old = *address_as_int64, assumed;
   do {
     assumed = old;
-    double assumed_double = *((double*)&assumed);
+    double assumed_double = *(reinterpret_cast<double*>(&assumed));
     double min = (val < assumed_double) ? val : assumed_double;
     old = __sync_val_compare_and_swap(address_as_int64, assumed,
-                                      *((int64_t*)&min));
+                                      *(reinterpret_cast<int64_t*>(&min)));
   } while (assumed != old);
-  return *((double *)&old);
+  return *(reinterpret_cast<double*>(&old));
 }
 
 /**
@@ -261,5 +241,16 @@ inline double stopwatch_elapsed(stopwatch_t* stopwatch) {
   gettimeofday(&tval, NULL);
   return ((tval.tv_sec * 1000 + tval.tv_usec/1000.0) - *stopwatch);
 }
+
+// Times the execution time of a function. This is typically used for debugging
+// and profiling purposes.
+#define STOPWATCH_FUNC(func)                                            \
+  do {                                                                  \
+    stopwatch_t stopwatch;                                              \
+    stopwatch_start(&stopwatch);                                        \
+    func;                                                               \
+    printf("%s\t%f\n", #func, stopwatch_elapsed(&stopwatch)/1000.0);    \
+    fflush(stdout);                                                     \
+  } while (0)
 
 #endif  // TOTEM_COMDEF_H
